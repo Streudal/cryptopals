c2ae7568aec6b1892615bb5941a0d8f2947f4316
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-05 11:14:29 -0500

    Yet another change to the default texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3.14159265359</span> App
+          <span className="text-[hsl(280,100%,70%)]">Cryptopal challenges</span> 
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -13,10 +13,9 @@ export default function HomePage() {
             href="https://create.t3.gg/en/usage/first-steps"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <h3 className="text-2xl font-bold">Challenges →</h3>
             <div className="text-lg">
-              Just the hardest stuff - Everything you don't need to know to set up your
-              database and authentication.
+              Click here to try the challenges
             </div>
           </Link>
           <Link
@@ -24,10 +23,9 @@ export default function HomePage() {
             href="https://create.t3.gg/en/introduction"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <h3 className="text-2xl font-bold">Scores →</h3>
             <div className="text-lg">
-              Learn less about Create T3 App, the libraries it doesn't use, and how not to
-              deploy it.
+              Click here to check your scores!
             </div>
           </Link>
         </div>
