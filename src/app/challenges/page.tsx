2962abbf169cff6dcb577a65a86196a338e949cd
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-08 12:14:06 -0500

    added challenge numbers to their individual pages

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -5,7 +5,7 @@ export default function Challenge() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          <span className="text-[hsl(280,100%,70%)]">Convert hex to base64</span> 
+          <span className="text-[hsl(280,100%,70%)]">1. Convert hex to base64</span> 
           <h3 className="text-2xl font-semibold tracking-widest"> errr muh gerrrrd</h3>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
