fcc1a31b9e754b4b60d5af12e3d6b602c26bd43f
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-08 11:28:02 -0500

    routing edit

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -11,7 +11,7 @@ export default function Challenge() {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
         <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="/"
+            href="/challenges"
             //target="_blank" // Include if you want new pages to open in a new tab
           >
             <h3 className="text-2xl font-bold">Back</h3>
