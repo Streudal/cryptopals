14fd2bd7ebc0e8a9385c9620152e76b3875aacc2
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-08 11:57:54 -0500

    maybe it's good

--- src/app/challenges/[id]/page.tsx
+++ src/app/challenges/[id]/page.tsx
@@ -22,7 +22,7 @@ export default function Challenge() {
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="/challenges/3"
+            href="/challenges/2/3"
             //target="_blank" // Include if you want new pages to open in a new tab
             >
             <h3 className="text-2xl font-bold">Next</h3>
