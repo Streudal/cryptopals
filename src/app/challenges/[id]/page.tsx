c8643b175c3bb5d541faf5932a8b256ee5bd55c6
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-08 11:28:58 -0500

    routing

--- src/app/challenges/[id]/page.tsx
+++ src/app/challenges/[id]/page.tsx
@@ -6,6 +6,8 @@ export default function Challenge() {
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           <span className="text-[hsl(280,100%,70%)]">Fixed XOR</span> 
+          <h3 className="text-2xl font-semibold tracking-widest"> errr muh gerrrrd</h3>
+
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
         <Link
