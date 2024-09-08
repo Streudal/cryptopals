9d8e6a8a6d1a90960909676e5019eb1c34032d6a
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-08 11:13:03 -0500

    navigation progress

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -5,8 +5,8 @@ export default function Challenge() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          <span className="text-[hsl(280,100%,70%)]">Crypto Challenge Set 1</span> 
-          <h3 className="text-2xl font-semibold tracking-widest"> fdsa</h3>
+          <span className="text-[hsl(280,100%,70%)]">Convert hex to base64</span> 
+          <h3 className="text-2xl font-semibold tracking-widest"> errr muh gerrrrd</h3>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
         <Link
@@ -21,7 +21,7 @@ export default function Challenge() {
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="/challenges/1"
+            href="/challenges/2"
             //target="_blank" // Include if you want new pages to open in a new tab
             >
             <h3 className="text-2xl font-bold">Next</h3>
