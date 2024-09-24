f7326948ca6f4a38a1062b7d26b4b1bab9cda5f7
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-09-24 16:10:26 -0500

    "types":["vitest/globals"], needed to be declared in the tsconfig file

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/*I couldn't find a vite.config.ts file. This is what would be in it if it existed though */
+
+///<reference types = "vitest"/>
+///<reference types = "vite/client"/>
+import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
+
+//https://vitejs.dev/config
+export default defineConfig({
+  plugins:[react()],
+  test:{
+    globals: true, //no importing things like 'it' or 'describe' in all our files
+    environment:'jsdom',
+    setupFiles:'./src/test/setup.ts',
+    //you might want to disable it, if you don't have tests that rely on it
+    //since parsing CSS is clow
+    css:true,//otherwise it won't parse your files!!!
+  },
+})
