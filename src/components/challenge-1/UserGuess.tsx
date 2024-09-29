6afadbf3354d87b5646095512a8dc3240f310bac
Tyler Streu <streuta@cdmsmith.com>
2024-09-29 15:03:55 -0500

    Added in more components and started on hooking logic up to user input.

---
+++ src/components/challenge-1/UserGuess.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export default function UserGuess() {
+  return (
+    <div>
+      <div>
+        <Label htmlFor="email">Your email address</Label>
+        <Input id="" />
+      </div>
+      <Button>
+
+      </Button>
+    </div>
+  );
+}
