0049a4f0d799d9baf2172aeb8078057c23439753
Tyler Streu <streuta@cdmsmith.com>
2024-09-29 13:59:00 -0500

    Updated scores page.

--- src/app/scores/page.tsx
+++ src/app/scores/page.tsx
@@ -17,16 +17,18 @@ export default function Scores({ params }: ChallengeListPageProps) {
 
   return (
     <div className="flex flex-col gap-10 pt-5">
-
       <h1 className='text-6xl'>
         Scores
       </h1>
 
       <div className='flex w-full border border-white rounded-md text-4xl gap-4 p-4'>
-        <h3 className=''>Overall Score:</h3>
-        <div>
-          <span></span>
-          <span></span>
+        <h3>Overall Score</h3>
+        <div className='dot-fill'>
+          <div className='flex gap-1'>
+            <span></span>
+            <span>/</span>
+            <span></span>
+          </div>
         </div>
       </div>
       <Accordion type="single" collapsible className='w-full'>
@@ -37,10 +39,17 @@ export default function Scores({ params }: ChallengeListPageProps) {
               <ul className='flex flex-col gap-6'>
                 {set.challenges.map((challenge) => (
                   <li key={`challenge-${challenge.id}`} className='w-full flex justify-between'>
-                    <span>{challenge.title}:</span>
-                    <div>
-                      { } / {challenge.points}
-                    </div>
+                    <p className='flex gap-4'>
+                      <span className='font-bold'>{challenge.title}:</span>
+                      <span>{challenge.subTitle}</span>
+                    </p>
+                    <p className="dot-fill">
+                      <div className='flex gap-1'>
+                        <span>{0}</span>
+                        <span>/</span>
+                        <span>{challenge.points}</span>
+                      </div>
+                    </p>
                   </li>
                 ))}
               </ul>
