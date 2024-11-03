6d76b9caab4ff9aa02dad476be83a17b7a67b746
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-11-03 16:15:29 -0600

    challenge 6 stuff

--- tests/challenge-6.test.tsx
+++ tests/challenge-6.test.tsx
@@ -4,19 +4,19 @@ import fs from 'fs';
 import path from 'path';
 import Solution from '@/components/sets/challenge-6/Solution';
 import { getItem, saveItem } from '@/lib/utils';
-import Challenge from '../';
+import Challenge from '@/components/sets/challenge-6/Challenge';
 
 
-test('Challenge 6: Decrypt AES-128 ECB', () => {
-  const result = 'wokka wokka!!!';
-  expect(result).toBeDefined();
-    // Read the encrypted file
-  const filePath = path.join(__dirname, '../public/assets/set-1-challenge-data-6.txt');
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-  const encryptedData = Buffer.from(fileContent, 'base64');
-  expect(fileContent).toBeDefined();
-  expect(encryptedData).toBeDefined();
-});
+//test('Challenge 6: Decrypt AES-128 ECB', () => {
+//   const result = 'wokka wokka!!!';
+//   expect(result).toBeDefined();
+//     // Read the encrypted file
+//   const filePath = path.join(__dirname, '../public/assets/set-1-challenge-data-6.txt');
+//   const fileContent = fs.readFileSync(filePath, 'utf-8');
+//   const encryptedData = Buffer.from(fileContent, 'base64');
+//   expect(fileContent).toBeDefined();
+//   expect(encryptedData).toBeDefined();
+// });
   // // Process the file content
   // const encryptedLines = fileContent
   //   .split('\n')
@@ -33,7 +33,7 @@ test('Challenge 6: Decrypt AES-128 ECB', () => {
     singleByteXorDecrypt,
     base64ToByteArray,
     decrypt
-  } from '@/components/sets/challenge-6/Solution';
+  } from '@/solutions/set-1-challenge-6';
   
   describe('Cryptography Functions', () => {
     describe('hammingDistance', () => {
