7dfeac1cef687c8041a662b69c7c860ac853df6b
Tyler Streu <streuta@cdmsmith.com>
2024-11-04 18:07:14 -0600

    File cleanup.

--- tests/solution.test.ts
+++
@@ -1,9 +0,0 @@
-import { getItem } from '@/lib/utils';
-import { describe, expect, test } from 'vitest';
-
-describe('Test Challenge Solutions', () => {
-  test('Challenge 1', () => {
-    expect(getItem('challenge-1')).toBeNull();
-  });
-
-});
